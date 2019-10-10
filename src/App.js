d02f12b162fdf343b0512db77d4d9a4217b1717d
Cesar Barahona <cesar-barahona@hotmail.com>
2019-10-09 22:31:11 -0600

    proyecto expertos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import './App.css';
+import Login from './Login';
+import AdminMenu from './AdminMenu';
+import Configuracion from './Configuracion';
+import Proyectos from './Proyectos';
+import Plantillas from './Plantillas';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      
+          <Router>
+            <div>
+            
+
+
+            <Route exact path="/" component={Login} />
+            <Route path="/AdminMenu" component={AdminMenu} />
+            <Route path="/Configuracion" component={Configuracion}/>
+            <Route path='/Proyectos' component={Proyectos}/>
+            <Route path='/Plantillas' component={Plantillas}/>
+            
+            
+          </div>
+        </Router>
+          
+    </React.Fragment>
   );
 }
+
 
 export default App;
